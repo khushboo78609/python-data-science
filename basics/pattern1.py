b912b9b06ee30c5c3d6ade80542a1e96c1125052
khushboo <khushboo226004@gmail.com>
2023-11-20 14:01:26 +0530

    loops

---
+++ basics/pattern1.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+
+pencolor('red')
+pensize(5)
+speed('fastest')
+
+    
+for i in range(6):
+     fd(100) 
+     lt(360/6)
+     for i in range(3):
+          fd(150)
+          lt(360/3)
+        
+
+ 
+hideturtle()
+mainloop()
